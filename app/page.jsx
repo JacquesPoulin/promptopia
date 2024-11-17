d07fcf38433d3d2ca4348fdca03b596b2d5af1d0
jacquesPoulin <jacques.poulin64@gmail.com>
2024-11-17 13:51:21 +0100

    MAJ CSS

--- app/page.jsx
+++ app/page.jsx
@@ -21,7 +21,7 @@ const Home = () => {
 		<section className='w-full flex-center flex-col'>
 			<h1 className='head_text text-center'>
 				Découvrir & Partager
-				<br className='max-md:hidden' />
+				<br />
 				<span className='orange_gradient text-center'>
 					Prompts générés par IA
 				</span>
